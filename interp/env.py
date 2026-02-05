9f31f3edfacde4d9307be0d6e912b20ca7e0fb04
Florent Michel <florent.m42@gmail.com>
2026-02-05 10:34:58 +0000

    add prototype NTT implementation

--- interp/env.py
+++ interp/env.py
@@ -1,6 +1,14 @@
 import sys
 
 from interp.fhetch_ast import Constant, ScalarLiteral, VectorLiteral
+from numpy import argsort, array
+
+# Root of unity used for the NTT (if None, use the default from Sympy)
+ROOTS_UNITY = {}
+
+# Ordering for the NTT output and iNTT input
+NTT_ORDERING = {}
+INTT_ORDERING = {}
 
 def builtin_write(obj):
     match obj:
@@ -11,6 +19,22 @@ def builtin_write(obj):
                 builtin_write(x)
         case Constant(_, _, value):
             builtin_write(value)
+        case other:
+            print("Unknown type", type(other), file=sys.stderr)
+            print(other)
+
+
+def builtin_print(obj):
+    match obj:
+        case ScalarLiteral(x):
+            print(x, end='')
+        case VectorLiteral(v):
+            for x in v:
+                builtin_print(x)
+                print(', ', end='')
+            print()
+        case Constant(_, _, value):
+            builtin_print(value)
         case other:
             print("Unknown type", type(other), file=sys.stderr)
             print(other)
@@ -36,11 +60,39 @@ def builtin_mul(lhs, rhs, q):
     # integers??
     return (lhs * rhs) % q
 
+def builtin_set_rou(ring_dimension: ScalarLiteral, modulus: ScalarLiteral, rou: ScalarLiteral):
+    """Set the global root of unity for this modulus"""
+    assert ring_dimension.value > 0
+    ROOTS_UNITY[(ring_dimension.value, modulus.value)] = rou.value
+    return
+
+def builtin_set_ntt_order(order: VectorLiteral):
+    """Set the NTT output order"""
+    ring_dimension = len(order.value)
+    NTT_ORDERING[ring_dimension] = array([x.value for x in order.value])
+    INTT_ORDERING[ring_dimension] = argsort(NTT_ORDERING[ring_dimension])
+    return
+
+def builtin_ntt(lhs: VectorLiteral, q: ScalarLiteral):
+    assert isinstance(lhs, VectorLiteral)
+    return lhs.forward_ntt(q, rou=ROOTS_UNITY.get((len(lhs.value), q.value), None),
+                              permutation=NTT_ORDERING.get(len(lhs.value), None))
+
+def builtin_intt(lhs: VectorLiteral, q: ScalarLiteral):
+    assert isinstance(lhs, VectorLiteral)
+    return lhs.inverse_ntt(q, rou=ROOTS_UNITY.get((len(lhs.value), q.value), None),
+                              permutation=INTT_ORDERING.get(len(lhs.value), None))
+
 
 def default_global():
     return {
         "write": builtin_write,
+        "print": builtin_print,
         "sr_addp": builtin_add,
         "sr_subp": builtin_sub,
         "sr_mulp": builtin_mul,
+        "sr_set_rou": builtin_set_rou,
+        "sr_set_ntt_order": builtin_set_ntt_order,
+        "sr_NTT": builtin_ntt,
+        "sr_iNTT": builtin_intt,
     }
--- interp/fhetch_ast.py
+++ interp/fhetch_ast.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass, field
 from enum import StrEnum
+from numpy import array
 from typing import Optional
+from sympy import ntt, intt
+from sympy.ntheory.residue_ntheory import nthroot_mod
 
+from ntt import _number_theoretic_transform
 
 def _eval_const_scalar(parse_result):
     match parse_result:
@@ -162,6 +166,34 @@ class ScalarLiteral(Expression):
         return other.mmuls(self, q)
 
 
+# Scratchpad to store powers of roots of unity
+# 
+# NOTE: This implementation assumes the root of unity is set the first time an (i)NTT is called 
+# for a given size and modulus. The same root is re-used for all subsequent (i)NTTs with the same 
+# dimension and modulus.
+class _NbTheoryScratchpad:
+    
+    def __init__(self):
+        self.powers_rou = {}
+    
+    def add_powers_rou(self, modulus: int, ring_dimension: int, rou: int = None):
+        if rou is None:
+            rou = nthroot_mod(modulus-1, ring_dimension, modulus)
+        w = 1
+        powers = [w]
+        for i in range(1, 2*ring_dimension):
+            w = (w * rou) % modulus
+            powers.append(w)
+        self.powers_rou[(modulus, ring_dimension)] = powers
+
+    def get_powers_rou(self, modulus: int, ring_dimension: int, rou: int = None):
+        if not (modulus, ring_dimension) in self.powers_rou: 
+            self.add_powers_rou(modulus, ring_dimension, rou=rou)
+        return self.powers_rou[(modulus, ring_dimension)]
+
+_nb_theory_scratchpad = _NbTheoryScratchpad()
+
+
 @dataclass
 class VectorLiteral(Expression):
     value: list[int]
@@ -191,6 +223,33 @@ class VectorLiteral(Expression):
     def mmulv(self, other, q):
         """Modular multiplication with Vector (element-wise)."""
         return VectorLiteral([(a * b) % q for a, b in zip(self.value, other.value)])
+    
+    def forward_ntt(self, q, rou=None, permutation=None):
+        """Forward NTT function"""
+        if permutation is None:
+            permutation = array(range(len(self.value)))
+        q = q.value
+        rou2 = (rou*rou)%q if rou is not None else None
+        coefficients = [x.value for x in self.value]
+        prefactors = _nb_theory_scratchpad.get_powers_rou(q, len(coefficients), rou=rou)
+        for (i,coefficient) in enumerate(coefficients[1:]):
+            coefficients[i+1] = (prefactors[i+1] * coefficient) % q
+        coefficients_ntt = _number_theoretic_transform(coefficients, q, rou=rou2, inverse=False)
+        return VectorLiteral(list(map(ScalarLiteral, 
+                                      [coefficients_ntt[permutation[i]] for i in range(len(coefficients_ntt))])))
+    
+    def inverse_ntt(self, q, rou=None, permutation=None):
+        """Inverse NTT function"""
+        if permutation is None:
+            permutation = array(range(len(self.value)))
+        q = q.value
+        rou2 = (rou*rou)%q if rou is not None else None
+        coefficients = [self.value[permutation[i]].value for i in range(len(self.value))]
+        coefficients_intt = _number_theoretic_transform(coefficients, q, rou=rou2, inverse=True)
+        prefactors = _nb_theory_scratchpad.get_powers_rou(q, len(coefficients))
+        for (i,coefficient) in enumerate(coefficients_intt[1:]):
+            coefficients_intt[i+1] = (prefactors[2*len(coefficients)-i-1] * coefficient) % q
+        return VectorLiteral(list(map(ScalarLiteral, coefficients_intt)))
 
 @dataclass
 class VarDefinition(Instruction):
